e8fdbb2d6e0b515f5a3d5733516dd9660fc0b5a5
Valerii Zhirnov <vazhirnov@yamoney.ru>
2019-03-25 12:27:47 +0300

    BACKEND-2586 Initial commit

---
+++ src/main/kotlin/ru/yandex/money/gradle/plugins/backend/build/JavaModulePlugin.kt
@@ -0,0 +1,15 @@
+package ru.yandex.money.gradle.plugins.backend.build
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Плагин для сборки модулей компонента
+ *
+ * @author Valerii Zhirnov (vazhirnov@yamoney.ru)
+ * @since 22.03.2019
+ */
+class JavaModulePlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+    }
+}
