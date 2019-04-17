0c8985f69345bca0a19243754ad091547dd5d79c
Valerii Zhirnov <vazhirnov@yamoney.ru>
2019-04-17 14:11:20 +0300

    BACKEND-2588 Перенёс код из build-idea-settings.gradle

--- src/main/kotlin/ru/yandex/money/gradle/plugins/backend/build/JavaModulePlugin.kt
+++ src/main/kotlin/ru/yandex/money/gradle/plugins/backend/build/JavaModulePlugin.kt
@@ -3,7 +3,9 @@ package ru.yandex.money.gradle.plugins.backend.build
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
+import org.gradle.plugins.ide.idea.IdeaPlugin
 import ru.yandex.money.gradle.plugins.backend.build.errorprone.ErrorProneConfigurer
+import ru.yandex.money.gradle.plugins.backend.build.idea.IdeaPluginConfigurer
 import ru.yandex.money.gradle.plugins.backend.build.jar.JarConfigurer
 
 /**
@@ -16,10 +18,12 @@ class JavaModulePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.buildDir = target.file("target")
         target.pluginManager.apply(JavaPlugin::class.java)
+        target.pluginManager.apply(IdeaPlugin::class.java)
 
         target.beforeEvaluate {
             JarConfigurer().init(it)
             ErrorProneConfigurer().init(it)
+            IdeaPluginConfigurer().init(it)
         }
     }
 }
