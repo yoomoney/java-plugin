7d1f0084fa436c07f6adaf1947b497ca355ec23e
Ivan Razgulin <ikrazgulin@yamoney.ru>
2019-12-05 10:13:25 +0300

    BACKEND-2879 Добавил sourceset grafana в список исключений при проверке зависимостей.

--- src/main/kotlin/ru/yandex/money/gradle/plugins/backend/build/checkdependencies/CheckDependenciesConfigurer.kt
+++ src/main/kotlin/ru/yandex/money/gradle/plugins/backend/build/checkdependencies/CheckDependenciesConfigurer.kt
@@ -70,7 +70,7 @@ class CheckDependenciesConfigurer {
         checkDependenciesPluginExtension!!.excludedConfigurations = Arrays.asList(
                 "checkstyle", "errorprone", "optional", "findbugs",
                 "architecture", "architectureTestCompile", "architectureTestCompileClasspath",
-                "architectureTestRuntime", "architectureTestRuntimeClasspath", "dockerJava,",
+                "architectureTestRuntime", "architectureTestRuntimeClasspath", "dockerJava",
                 "grafanaDashboardsCompile", "grafana", "grafanaCompile", "grafanaCompileClasspath",
                 "grafanaRuntime", "grafanaRuntimeClasspath")
 
