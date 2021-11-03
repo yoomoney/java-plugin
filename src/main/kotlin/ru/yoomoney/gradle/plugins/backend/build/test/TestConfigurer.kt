14016ecb2e2ebd20c10ac62ad49bbf55323a6ac6
akonyshev <akonyshev@yoomoney.ru>
2021-11-03 12:37:56 +0300

    BACKEND-5056 Исправление дублирования таски test

--- src/main/kotlin/ru/yoomoney/gradle/plugins/backend/build/test/TestConfigurer.kt
+++ src/main/kotlin/ru/yoomoney/gradle/plugins/backend/build/test/TestConfigurer.kt
@@ -1,6 +1,5 @@
 package ru.yoomoney.gradle.plugins.backend.build.test
 
-import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.SourceSet
